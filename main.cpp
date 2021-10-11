684b23ddbb612c531587a0bfa165e0009b08a545
philsmeby <phil.smeby@gmail.com>
2021-10-11 08:43:52 -0400

    Added player pawn to game

--- main.cpp
+++ main.cpp
@@ -11,6 +11,12 @@ int main()
 	Vector2 mapPos{0.0, 0.0};
 	float speed{4.0f};
 
+	Texture2D knight = LoadTexture("assets/characters/knight_idle_spritesheet.png");
+	Vector2 knightPos{
+			(float)windowWidth/2.0f - 4.0f * (0.5f * (float)knight.width/6.0f),
+			(float)windowHeight/2.0f - 4.0f * (0.5f * (float)knight.height)
+	};
+
 	SetTargetFPS(60);
 
 	// Game loops
@@ -32,7 +38,12 @@ int main()
 			mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
 		}
 
+		// Draw Game assets
 		DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+		Rectangle source{0.f, 0.f, (float)knight.width/6.0f, (float)knight.height};
+		Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f,  4.0f * (float)knight.height};
+
+		DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
 
 		EndDrawing();
 	}
