f36b96fac2d73f80eaf6d525de5c85f97a8496b4
philsmeby <phil.smeby@gmail.com>
2021-10-21 00:37:04 -0400

    health displayed on screen

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "character.h"
 #include "Prop.h"
 #include "enemy.h"
+#include <string>
 
 int main()
 {
@@ -44,6 +45,19 @@ int main()
 		for (auto prop : props)
 		{
 			prop.Render(knight.getWorldPos());
+		}
+
+		if (!knight.getAlive())
+		{
+			DrawText("Game Over!", 55.f, 45.f, 40, RED);
+			EndDrawing();
+			continue;
+		}
+		else
+		{
+			std::string knightsHealth = "Health: ";
+			knightsHealth.append(std::to_string(knight.getHealth()), 0, 5);
+			DrawText(knightsHealth.c_str(), 55.f, 45.f, 40, RED);
 		}
 
 		// logic to check map boundaries
